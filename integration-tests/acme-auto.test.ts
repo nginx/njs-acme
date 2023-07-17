67f0a79cd4df73e6b39d4a8e8452ab09eb55aeaf
Maxim Ivanitskiy <m.ivanitskiy@f5.com>
2023-07-17 12:34:48 -0700

    fix: integration e2e test

---
+++ integration-tests/acme-auto.test.ts
@@ -0,0 +1,51 @@
+import { strict as assert } from 'assert'
+import { test } from 'mocha'
+import './hooks'
+
+interface CertificateData {
+  certificate: {
+    issuer: {
+      [key: string]: string
+    }[]
+    domains: {
+      commonName: string
+      altNames: string[][]
+    }
+    notBefore: string
+    notAfter: string
+  }
+  renewedCertificate: boolean
+}
+
+describe('Integration:AutoMode', async function () {
+  it('issue cert', async function () {
+    this.timeout('10s')
+    const resp = await this.client.get('')
+    assert.equal(resp.statusCode, 200)
+    const respBody = JSON.parse(resp.body)
+    assert.equal(respBody.server_name, this.nginxHost)
+    assert.equal(respBody.ssl_session_id, '')
+
+    const respCert = await this.client.get('acme/auto')
+    assert.equal(respCert.statusCode, 200)
+    const certInfo = JSON.parse(respCert.body) as CertificateData
+    assert(
+      certInfo.certificate.domains.commonName.includes('Pebble Intermediate CA')
+    )
+    assert.equal(certInfo.certificate.domains.altNames.length, 1)
+    assert.equal(certInfo.certificate.domains.altNames[0][0], this.nginxHost)
+
+    const httpsClient = this.client.extend({
+      prefixUrl: `https://${this.nginxHost}:${this.nginx.ports[1]}`,
+      https: {
+        rejectUnauthorized: false,
+      },
+    })
+
+    const httpsResp = await httpsClient.get('')
+    assert.equal(httpsResp.statusCode, 200)
+    const httpsBody = JSON.parse(httpsResp.body)
+    assert.equal(httpsBody.server_name, this.nginxHost)
+    assert.notEqual(httpsBody.ssl_session_id, '')
+  })
+})
--- integration-tests/hooks.ts
+++ integration-tests/hooks.ts
@@ -2,67 +2,87 @@
 import got, { Got } from 'got'
 import { Context, RootHookObject } from 'mocha'
 import { beforeEachSuite } from 'mocha-suite-hooks'
-import { startNginx, NginxServer } from 'nginx-testing'
+import { startNginx, NginxServer, NginxOptions } from 'nginx-testing'
 
 const nginxConfig = `${__dirname}/nginx.conf`
-const nginxVersion = process.env.NGINX_VERSION || '1.22.x'
-const host = '127.0.0.1'
+// use specified "local" already insatlled nginx (e.g. /usr/sbin/nginx)
+// instead of pulling nginx binary from nginx-testing lib
+const useNginxBinPath = process.env.USE_NGINX_BIN_PATH
+// let nginx-testing to know which nginx to download (not official builds)
+const nginxVersion = process.env.NGINX_VERSION || '1.25.x'
+//
+export const host = process.env.NGINX_HOSTNAME || '127.0.0.1'
 
 declare module 'mocha' {
-    export interface Context {
-        client: Got
-        nginx: NginxServer
-    }
+  export interface Context {
+    client: Got
+    nginx: NginxServer
+    nginxHost: string
+  }
 }
 
-
 export const mochaHooks: RootHookObject = {
-    async beforeAll(this: Context) {
-        this.timeout(30_000)
+  async beforeAll(this: Context) {
+    this.timeout(30_000)
+    this.nginxHost = host
 
-        this.nginx = await startNginx({ version: nginxVersion, bindAddress: host, configPath: nginxConfig })
+    const opts = {
+      bindAddress: this.nginxHost,
+      configPath: nginxConfig,
+      ports: [8000, 4443],
+      workDir: __dirname,
+    } as NginxOptions
 
-        const errors = (await this.nginx.readErrorLog())
-            .split('\n')
-            .filter(line => line.includes('[error]'))
-        if (errors) {
-            console.error(errors.join('\n'))
-        }
-
-        this.client = got.extend({
-            prefixUrl: `http://${host}:${this.nginx.port}`,
-            throwHttpErrors: false,
-        })
-
-        beforeEachSuite(async function () {
-            // Read the logs to consume (discard) them before running next test suite
-            // (describe block).
-            await this.nginx.readErrorLog()
-            await this.nginx.readAccessLog()
-        })
-    },
-
-    async afterAll(this: Context) {
-        if (this.nginx) {
-            await this.nginx.stop()
-        }
-    },
-
-    async afterEach(this: Context) {
-        const { currentTest, nginx } = this
-
-        if (currentTest?.state === 'failed' && currentTest.err) {
-            const errorLog = await nginx.readErrorLog()
-            const accessLog = await nginx.readAccessLog()
-
-            const logs = [
-                errorLog && '----- Error Log -----\n' + errorLog,
-                accessLog && '----- Access Log -----\n' + accessLog,
-            ].filter(Boolean)
-
-            if (logs.length > 0) {
-                currentTest.err.stack += '\n\n' + logs.join('\n\n').replace(/^/gm, '    ')
-            }
-        }
+    if (useNginxBinPath) {
+      opts.binPath = useNginxBinPath
+    } else {
+      opts.version = nginxVersion
     }
+    console.info('running tests in folder', process.cwd())
+    console.info('using opts', opts)
+    this.nginx = await startNginx(opts)
+    console.info('nginx config\n', this.nginx.config)
+
+    const errors = (await this.nginx.readErrorLog())
+      .split('\n')
+      .filter((line) => line.includes('[error]'))
+    if (errors) {
+      console.error(errors.join('\n'))
+    }
+
+    this.client = got.extend({
+      prefixUrl: `http://${this.nginxHost}:${this.nginx.port}`,
+      throwHttpErrors: false,
+    })
+
+    beforeEachSuite(async function () {
+      await this.nginx.readErrorLog()
+      await this.nginx.readAccessLog()
+    })
+  },
+
+  async afterAll(this: Context) {
+    if (this.nginx) {
+      await this.nginx.stop()
+    }
+  },
+
+  async afterEach(this: Context) {
+    const { currentTest, nginx } = this
+
+    if (currentTest?.state === 'failed' && currentTest.err) {
+      const errorLog = await nginx.readErrorLog()
+      const accessLog = await nginx.readAccessLog()
+
+      const logs = [
+        errorLog && '----- Error Log -----\n' + errorLog,
+        accessLog && '----- Access Log -----\n' + accessLog,
+      ].filter(Boolean)
+
+      if (logs.length > 0) {
+        currentTest.err.stack +=
+          '\n\n' + logs.join('\n\n').replace(/^/gm, '    ')
+      }
+    }
+  },
 }
